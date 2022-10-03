5ba4214018bad5cd1d15843900cd5e8156ec55b2
bela333 <b3kstudio@gmail.com>
2022-10-03 22:28:37 +0200

    Basic bot functionality

--- database/surprise_day.py
+++ database/surprise_day.py
@@ -14,30 +14,36 @@ def random_surprise_day(now):
 
     t = random.random()
 
-    surprise_day = datetime.fromtimestamp(now.timestamp()*t+next_year.timestamp()*(1-t))
+    surprise_day = normalize_datetime(datetime.fromtimestamp(now.timestamp()*t+next_year.timestamp()*(1-t)))
     return surprise_day
 
 class SurpriseDay:
-    def __init__(self, id: int, discord: str, surprise_day: datetime, reset_day: datetime) -> None:
+    def __init__(self, id: int, discord: str, channel: str, surprise_day: datetime, reset_day: datetime) -> None:
         self.id = id
         self.discord = discord
+        self.channel = channel
         self.surprise_day = surprise_day
         self.reset_day = reset_day
     
     def create(self, database: Connection) -> None:
         cur = database.cursor()
-        res = cur.execute("INSERT INTO surprise_days(discord, surprise_day, reset_day) VALUES (?,?,?)", [self.discord, self.surprise_day.timestamp(), self.reset_day.timestamp()])
+        res = cur.execute("INSERT INTO surprise_days(discord, channel, surprise_day, reset_day) VALUES (?,?,?,?)", [self.discord,self.channel, self.surprise_day.timestamp(), self.reset_day.timestamp()])
         self.id = res.lastrowid
         database.commit()
     
+    def update_channel(self, database: Connection, channel: str):
+        database.execute("UPDATE surprise_days SET channel = ? WHERE id = ?", (channel, self.id))
+        database.commit()
+        self.channel = channel
+    
     def get_from_discord(database: Connection, discord: str) -> SurpriseDay | None:
         cur = database.cursor()
-        res = cur.execute("""SELECT id, discord, surprise_day, reset_day FROM surprise_days WHERE "discord"=?;""", [discord])
+        res = cur.execute("""SELECT id, discord, channel, surprise_day, reset_day FROM surprise_days WHERE "discord"=?;""", [discord])
         res = res.fetchone()
         if res == None:
             return None
-        id, discord, surprise_day, reset_day = res
-        return SurpriseDay(id, discord, datetime.fromtimestamp(surprise_day), datetime.fromtimestamp(reset_day))
+        id, discord, channel, surprise_day, reset_day = res
+        return SurpriseDay(id, discord, channel, datetime.fromtimestamp(surprise_day), datetime.fromtimestamp(reset_day))
 
     def get_from_discord_or_default(database: Connection, discord: str) -> SurpriseDay:
         res = SurpriseDay.get_from_discord(database, discord)
@@ -47,7 +53,7 @@ class SurpriseDay:
         now = normalize_datetime(datetime.now())
         surprise_day = random_surprise_day(now)
         reset_day = now + timedelta(days=365)
-        res = SurpriseDay(0, discord, surprise_day, reset_day)
+        res = SurpriseDay(0, discord, None, surprise_day, reset_day)
         
         res.create(database)
         return res
@@ -61,7 +67,9 @@ class SurpriseDay:
         database.execute("""CREATE TABLE IF NOT EXISTS "surprise_days" (
         "id"	INTEGER,
         "discord"	TEXT,
+        "channel"	TEXT,
         "surprise_day"	INTEGER,
         "reset_day"	INTEGER,
         PRIMARY KEY("id" AUTOINCREMENT)
-    );""")
+        );""")
+        database.commit()
