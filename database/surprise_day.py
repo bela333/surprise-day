e9a8fe7b9e482687dce8306b03119d915837904a
bela333 <b3kstudio@gmail.com>
2022-10-04 11:43:57 +0200

    Formatting

--- database/surprise_day.py
+++ database/surprise_day.py
@@ -3,19 +3,22 @@ from datetime import datetime, timedelta
 import random
 from sqlite3 import Connection
 
+
 def normalize_datetime(t: datetime):
     return t.replace(hour=0, minute=0, second=0)
+
 
 def random_surprise_day(now):
     now = normalize_datetime(now)
 
     next_year = now.replace(year=now.year + 1)
-    next_year = next_year-timedelta(days=1)
+    next_year = next_year - timedelta(days=1)
 
     t = random.random()
 
-    surprise_day = normalize_datetime(datetime.fromtimestamp(now.timestamp()*t+next_year.timestamp()*(1-t)))
+    surprise_day = normalize_datetime(datetime.fromtimestamp(now.timestamp() * t + next_year.timestamp() * (1 - t)))
     return surprise_day
+
 
 class SurpriseDay:
     def __init__(self, id: int, discord: str, channel: str, surprise_day: datetime, reset_day: datetime) -> None:
@@ -24,35 +27,46 @@ class SurpriseDay:
         self.channel = channel
         self.surprise_day = surprise_day
         self.reset_day = reset_day
-    
+
     def create(self, database: Connection) -> None:
         cur = database.cursor()
-        res = cur.execute("INSERT INTO surprise_days(discord, channel, surprise_day, reset_day) VALUES (?,?,?,?)", [self.discord,self.channel, self.surprise_day.timestamp(), self.reset_day.timestamp()])
+        res = cur.execute(
+            "INSERT INTO surprise_days(discord, channel, surprise_day, reset_day) VALUES (?,?,?,?)",
+            [self.discord, self.channel, self.surprise_day.timestamp(), self.reset_day.timestamp()],
+        )
         self.id = res.lastrowid
         database.commit()
-    
+
     def update_channel(self, database: Connection, channel: str):
         database.execute("UPDATE surprise_days SET channel = ? WHERE id = ?", (channel, self.id))
         database.commit()
         self.channel = channel
-    
+
     def get_from_channel(database: Connection, channel: str) -> SurpriseDay | None:
         cur = database.cursor()
-        res = cur.execute("""SELECT id, discord, channel, surprise_day, reset_day FROM surprise_days WHERE "channel"=?;""", [channel])
+        res = cur.execute(
+            """SELECT id, discord, channel, surprise_day, reset_day FROM surprise_days WHERE "channel"=?;""", [channel]
+        )
         res = res.fetchone()
         if res == None:
             return None
         id, discord, channel, surprise_day, reset_day = res
-        return SurpriseDay(id, discord, channel, datetime.fromtimestamp(surprise_day), datetime.fromtimestamp(reset_day))
+        return SurpriseDay(
+            id, discord, channel, datetime.fromtimestamp(surprise_day), datetime.fromtimestamp(reset_day)
+        )
 
     def get_from_discord(database: Connection, discord: str) -> SurpriseDay | None:
         cur = database.cursor()
-        res = cur.execute("""SELECT id, discord, channel, surprise_day, reset_day FROM surprise_days WHERE "discord"=?;""", [discord])
+        res = cur.execute(
+            """SELECT id, discord, channel, surprise_day, reset_day FROM surprise_days WHERE "discord"=?;""", [discord]
+        )
         res = res.fetchone()
         if res == None:
             return None
         id, discord, channel, surprise_day, reset_day = res
-        return SurpriseDay(id, discord, channel, datetime.fromtimestamp(surprise_day), datetime.fromtimestamp(reset_day))
+        return SurpriseDay(
+            id, discord, channel, datetime.fromtimestamp(surprise_day), datetime.fromtimestamp(reset_day)
+        )
 
     def get_from_discord_or_default(database: Connection, discord: str) -> SurpriseDay:
         res = SurpriseDay.get_from_discord(database, discord)
@@ -63,22 +77,22 @@ class SurpriseDay:
         surprise_day = random_surprise_day(now)
         reset_day = now + timedelta(days=365)
         res = SurpriseDay(0, discord, None, surprise_day, reset_day)
-        
+
         res.create(database)
         return res
-    
-
 
     def __repr__(self) -> str:
         return "{} {} {} {}".format(self.id, self.discord, self.surprise_day, self.reset_day)
 
     def setup(database: Connection) -> None:
-        database.execute("""CREATE TABLE IF NOT EXISTS "surprise_days" (
+        database.execute(
+            """CREATE TABLE IF NOT EXISTS "surprise_days" (
         "id"	INTEGER,
         "discord"	TEXT,
         "channel"	TEXT,
         "surprise_day"	INTEGER,
         "reset_day"	INTEGER,
         PRIMARY KEY("id" AUTOINCREMENT)
-        );""")
+        );"""
+        )
         database.commit()
--- main.py
+++ main.py
@@ -7,34 +7,39 @@ from database.surprise_day import SurpriseDay
 
 
 load_dotenv()
-#TODO: Check variables. TOKEN, CATEGORY, GUILD
+# TODO: Check variables. TOKEN, CATEGORY, GUILD
+
 
 def is_debug() -> bool:
     d = os.getenv("DEBUG")
-    return d == '1'
+    return d == "1"
+
 
 database = sqlite3.connect("database.db")
 
+
 def setup():
     SurpriseDay.setup(database)
-    
 
 
 bot = hikari.GatewayBot(token=os.getenv("TOKEN"), intents=hikari.Intents.GUILD_MEMBERS)
 
 setup()
 
+
 @bot.listen()
 async def register_commands(event: hikari.StartingEvent) -> None:
     application = await bot.rest.fetch_application()
-    
+
     commands = [
-        bot.rest.slash_command_builder("join", "Join in someone else's celebration!").add_option(hikari.CommandOption(type=hikari.OptionType.USER, name="user", description="User",is_required=True)),
-        bot.rest.slash_command_builder("leave", "Leave someone else's channel ;(")
+        bot.rest.slash_command_builder("join", "Join in someone else's celebration!").add_option(
+            hikari.CommandOption(type=hikari.OptionType.USER, name="user", description="User", is_required=True)
+        ),
+        bot.rest.slash_command_builder("leave", "Leave someone else's channel ;("),
     ]
     debug_commands = [
-        bot.rest.slash_command_builder("forcejoin", "Runs the \"on_join\" event"),
-        bot.rest.slash_command_builder("forceleave", "Runs the \"on_leave\" event")
+        bot.rest.slash_command_builder("forcejoin", 'Runs the "on_join" event'),
+        bot.rest.slash_command_builder("forceleave", 'Runs the "on_leave" event'),
     ]
 
     if is_debug():
@@ -42,57 +47,88 @@ async def register_commands(event: hikari.StartingEvent) -> None:
 
     await bot.rest.set_application_commands(application=application.id, commands=commands, guild=os.getenv("GUILD"))
 
+
 @bot.listen()
 async def handle_interactions(event: hikari.InteractionCreateEvent) -> None:
     if not isinstance(event.interaction, hikari.CommandInteraction):
         return
-    
+
     if is_debug():
         if event.interaction.command_name == "forcejoin":
-            await event.interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, "Good!", flags=hikari.MessageFlag.EPHEMERAL)
+            await event.interaction.create_initial_response(
+                hikari.ResponseType.MESSAGE_CREATE, "Good!", flags=hikari.MessageFlag.EPHEMERAL
+            )
             await handle_join(event.interaction.member, await event.interaction.fetch_guild())
         if event.interaction.command_name == "forceleave":
-            await event.interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, "Good!", flags=hikari.MessageFlag.EPHEMERAL)
+            await event.interaction.create_initial_response(
+                hikari.ResponseType.MESSAGE_CREATE, "Good!", flags=hikari.MessageFlag.EPHEMERAL
+            )
             await handle_leave(event.interaction.member.id, await event.interaction.fetch_guild())
         if event.interaction.command_name == "join":
             userid = event.interaction.options[0].value
             if userid != event.interaction.member.id:
                 day = SurpriseDay.get_from_discord(database, userid)
                 if day is None or day.channel is None:
-                    await event.interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, "Unknown error", flags=hikari.MessageFlag.EPHEMERAL)
+                    await event.interaction.create_initial_response(
+                        hikari.ResponseType.MESSAGE_CREATE, "Unknown error", flags=hikari.MessageFlag.EPHEMERAL
+                    )
                     return
                 channel = await bot.rest.fetch_channel(day.channel)
                 if channel is None or not isinstance(channel, hikari.GuildTextChannel):
-                    await event.interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, "Oops", flags=hikari.MessageFlag.EPHEMERAL)
+                    await event.interaction.create_initial_response(
+                        hikari.ResponseType.MESSAGE_CREATE, "Oops", flags=hikari.MessageFlag.EPHEMERAL
+                    )
                     return
-                await channel.edit_overwrite(event.interaction.member.id, target_type=hikari.PermissionOverwriteType.MEMBER, allow=hikari.Permissions.VIEW_CHANNEL)
-                await event.interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, "Joined!!", flags=hikari.MessageFlag.EPHEMERAL)
+                await channel.edit_overwrite(
+                    event.interaction.member.id,
+                    target_type=hikari.PermissionOverwriteType.MEMBER,
+                    allow=hikari.Permissions.VIEW_CHANNEL,
+                )
+                await event.interaction.create_initial_response(
+                    hikari.ResponseType.MESSAGE_CREATE, "Joined!!", flags=hikari.MessageFlag.EPHEMERAL
+                )
             else:
-                await event.interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, "You can't choose yourself", flags=hikari.MessageFlag.EPHEMERAL)
+                await event.interaction.create_initial_response(
+                    hikari.ResponseType.MESSAGE_CREATE, "You can't choose yourself", flags=hikari.MessageFlag.EPHEMERAL
+                )
         if event.interaction.command_name == "leave":
             day = SurpriseDay.get_from_channel(database, str(event.interaction.channel_id))
             if day is None:
-                await event.interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, "You are not in a celebratory channel", flags=hikari.MessageFlag.EPHEMERAL)
+                await event.interaction.create_initial_response(
+                    hikari.ResponseType.MESSAGE_CREATE,
+                    "You are not in a celebratory channel",
+                    flags=hikari.MessageFlag.EPHEMERAL,
+                )
                 return
             channel = await bot.rest.fetch_channel(day.channel)
             if channel is None or not isinstance(channel, hikari.GuildTextChannel):
-                    await event.interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, "Oops", flags=hikari.MessageFlag.EPHEMERAL)
-                    return
+                await event.interaction.create_initial_response(
+                    hikari.ResponseType.MESSAGE_CREATE, "Oops", flags=hikari.MessageFlag.EPHEMERAL
+                )
+                return
             await channel.remove_overwrite(event.interaction.member.id)
-            await event.interaction.create_initial_response(hikari.ResponseType.MESSAGE_CREATE, "Successfully left", flags=hikari.MessageFlag.EPHEMERAL)
+            await event.interaction.create_initial_response(
+                hikari.ResponseType.MESSAGE_CREATE, "Successfully left", flags=hikari.MessageFlag.EPHEMERAL
+            )
+
 
 async def handle_join(member: hikari.Member, guild: hikari.GatewayGuild) -> None:
     day = SurpriseDay.get_from_discord_or_default(database, member.id)
     if day.channel is not None:
-        #TODO: Cleanup channel
+        # TODO: Cleanup channel
         pass
     channel = await guild.create_text_channel(member.username, category=os.getenv("CATEGORY"))
 
-    await channel.edit_overwrite(guild.id, deny=hikari.Permissions.VIEW_CHANNEL, target_type=hikari.PermissionOverwriteType.ROLE)
+    await channel.edit_overwrite(
+        guild.id, deny=hikari.Permissions.VIEW_CHANNEL, target_type=hikari.PermissionOverwriteType.ROLE
+    )
     day.update_channel(database, str(channel.id))
 
-    msg = await channel.send("<@{0}>'s Surprise Day is on <t:{1}>, <t:{1}:R>".format(member.id, int(day.surprise_day.timestamp())))
+    msg = await channel.send(
+        "<@{0}>'s Surprise Day is on <t:{1}>, <t:{1}:R>".format(member.id, int(day.surprise_day.timestamp()))
+    )
     await channel.pin_message(msg.id)
+
 
 async def handle_leave(user_id: hikari.Snowflake, guild: hikari.GatewayGuild) -> None:
     day = SurpriseDay.get_from_discord(database, str(user_id))
@@ -101,21 +137,22 @@ async def handle_leave(user_id: hikari.Snowflake, guild: hikari.GatewayGuild) ->
     await guild.delete_channel(day.channel)
     day.update_channel(database, None)
 
+
 @bot.listen()
 async def on_join(event: hikari.MemberCreateEvent) -> None:
     if event.member.is_bot:
-        #Skip bots
+        # Skip bots
         return
-    #TODO: Cached value might be None
+    # TODO: Cached value might be None
     await handle_join(event.member, event.get_guild())
+
 
 @bot.listen()
 async def on_leave(event: hikari.MemberDeleteEvent) -> None:
     if event.old_member is not None and event.old_member.is_bot:
         return
-    #TODO: Cached value might be None
+    # TODO: Cached value might be None
     await handle_leave(event.user_id, event.get_guild())
-    
-    
+
 
 bot.run()
